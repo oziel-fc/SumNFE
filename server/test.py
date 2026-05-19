2c3e8bd83e085a72e793c75ac6264778b821c149
oziel-fc <ozielfc.oficial@gmail.com>
2026-05-19 20:14:08 -0300

    Remove test.py do versionamento

--- server/test.py
+++
@@ -1,9 +0,0 @@
-import pdfplumber
-
-with pdfplumber.open("arquivo.pdf") as pdf:
-    for page in pdf.pages:
-        tables = page.extract_tables()
-
-        for table in tables:
-            for row in table:
-                print(row)
