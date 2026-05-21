32b275d93976be1635f557411d7e40c1048949e8
oziel-fc <ozielfc.oficial@gmail.com>
2026-05-20 23:34:58 -0300

    minor quests

--- server/main.py
+++ server/main.py
@@ -11,7 +11,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://sumnfe.vercel.app/"
+        "https://sumnfe.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
