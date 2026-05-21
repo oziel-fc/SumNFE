ebcc0cfbaa94867c6245d96127e888f0290e1d82
oziel-fc <ozielfc.oficial@gmail.com>
2026-05-20 23:30:40 -0300

    Allowing the URL of vercel frontend on main.py

--- server/main.py
+++ server/main.py
@@ -10,7 +10,8 @@ app.add_middleware(SecurityMiddleware)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173"
+        "http://localhost:5173",
+        "https://sumnfe.vercel.app/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
